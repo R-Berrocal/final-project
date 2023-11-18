110bcdd55ef9e6dc44b3fb89ced5c548591cf5f4
Roberto Berrocal <robertoberro20@gmail.com>
2023-11-18 10:56:08 -0500

    refactor getOptionsByQuestionId and example in test.php add

--- test.php
+++ test.php
@@ -3,7 +3,20 @@
 require_once(__DIR__ . './models/questions.php');
 $questionModel = new QuestionModel();
 
-$questionary = $questionModel->findAll("SELECT * FROM questionary");
+// Traerse el cuestionario, solo deben pasar el ide del cuestionario
+$questionary = $questionModel->getQuestionaryById(1);
 
+// Traerse las preguntas del cuestionario pasando el id
+$questions = $questionModel->getQuestionsByQuestionaryId(1);
+
+// Traerse las opciones de cada una de las preguntas pasando
+foreach ($questions as &$question) {
+    $question['options'] = $questionModel->getOptionsByQuestionId($question['id']);
+}
+
+$result = array(
+    'questionary' => $questionary,
+    'questions' => $questions,
+);
 header("Content-Type: application/json");
-print_r(json_encode($questionary));
+print_r(json_encode($result));
