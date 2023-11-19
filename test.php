3ad783090fbcdec10d548de10c7466dd8b2768b9
camilo-a17 <camilko99a@gmail.com>
2023-11-19 11:13:02 -0500

    cambios en la forma de mostrar las preguntas y guardar las respuestas  por usuario

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . './models/questions.php');
 $questionModel = new QuestionModel();
 
 // Traerse el cuestionario, solo deben pasar el ide del cuestionario
-$questionary = $questionModel->getQuestionaryById(1);
+$questionary = $questionModel->getQuestionaryById(3);
 
 header('Content-Type: application/json');
 print_r(json_encode($questionary, JSON_PRETTY_PRINT));
