7a955889a46863d22ab89d672e91f92730b62663
Roberto Berrocal <robertoberro20@gmail.com>
2023-11-21 15:23:14 -0500

    refactor login.php

--- controllers/login.php
+++ controllers/login.php
@@ -4,23 +4,23 @@ session_start();
 require_once(__DIR__ . '../../models/user.php');
 
 if (isset($_POST)) {
-    
+
     $usuario = new UserModel();
     $usuario->setEmail($_POST['email']);
     $usuario->setPassword($_POST['password']);
 
     $identity = $usuario->login();
-    
+
     if ($identity && is_object($identity)) {
         if ($identity->role == 'user') {
-            $_SESSION['user'] = $identity;           
-
-        }else if ($identity->role == 'admin') {
+            $_SESSION['user'] = $identity;
+            header("Location: /final-project/views/");
+        } else if ($identity->role == 'admin') {
             $_SESSION['admin'] = $identity;
+            header("Location: /final-project/views/admin/dashboard.php");
         }
     } else {
         $_SESSION['error_login'] = 'Identificación fallida !!';
+        header("Location: /final-project/");
     }
 }
-
-header("Location: /final-project/views/");
